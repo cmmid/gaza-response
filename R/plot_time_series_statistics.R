16befb23b9e84d2397b0292c8573d93d48be617c
kathsherratt <kathsherratt@gmail.com>
2025-07-31 18:22:09 +0100

    add @clarabrigitta plot code
    Co-Authored-By: clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>

---
+++ R/plot_time_series_statistics.R
@@ -0,0 +1,70 @@
+#...............................................................................
+### ++ MONITORING THE NUTRITIONAL STATUS OF HUMANITARIAN WORKERS IN GAZA +++ ###
+#...............................................................................
+
+#...............................................................................
+## ------ R SCRIPT TO GENERATE PLOTS OF TIME SERIES SUMMARY STATISTICS ----- ##
+#...............................................................................
+
+#...............................................................................
+### Preparatory steps
+#...............................................................................
+
+#...................................      
+## Install or load required R packages
+if (!"pacman" %in% rownames(installed.packages())){install.packages("pacman")}
+
+# Install or load packages from CRAN
+pacman::p_load(
+  ggplot2,       # Visualise data
+  tidyverse)     # Tidyverse suite of packages
+
+#...................................      
+## Starting setup
+
+# Clean up from previous code / runs
+rm(list=ls(all=TRUE) )
+
+# Set working directory to where this file is stored
+dir_path <- paste(dirname(rstudioapi::getActiveDocumentContext()$path  )
+                  , "/", sep = "")
+setwd(dir_path)
+print( getwd() )
+
+#...............................................................................
+### Read in current summary statistics data
+#...............................................................................
+
+time_series_stats_filename <- "time_series_table.csv"
+time_series_stats <- read.csv(time_series_stats_filename)
+
+#...............................................................................
+### Plot
+#...............................................................................
+
+plot_time_series_statistics <- function(data, option = c("Overall", "Sex", "Age Group", "Governorate", "Role")){
+  
+  # Filter data for the selected option
+  data <- data %>% 
+    filter(Stratification == option) %>% 
+    mutate(Variable = factor(Variable, levels = c("% Weight Change from Prewar Value", "% Weight Change from First Measurement", "Weight (kg)", "BMI")))
+  
+  # Generate plot
+  fig <- data %>% 
+    ggplot(aes(x = Date)) +
+    geom_line(aes(y = mean, group = Variable, colour = "Mean"), linetype = "solid") +
+    geom_line(aes(y = median, group = Variable, colour = "Median"), linetype = "dashed") +
+    geom_ribbon(aes(ymin = q1, ymax = q3, group = Variable, fill = "IQR"), alpha = 0.2, linetype = 0) +
+    scale_colour_manual(values = c("Mean" = "darkred", "Median" = "darkblue")) +
+    scale_fill_manual(values = c("IQR" = "darkblue")) +
+    facet_grid(cols = vars(Group), row = vars(Variable), switch = "y", labeller = label_wrap_gen(width = 25), scales = "free") +
+    labs(x = "Date", y = "Value") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+          legend.title     = element_blank(),        
+          legend.spacing.y = unit(2, "pt"),           
+          legend.margin    = margin(2, 2, 2, 2))
+  
+  return(fig)
+  
+}

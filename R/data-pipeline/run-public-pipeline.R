3f8821ddf78a34afd1add2e248d22aa0fe6ffd2c
kathsherratt <kathsherratt@gmail.com>
2025-08-05 12:34:49 +0100

    Create run-public-pipeline.R

---
+++ R/data-pipeline/run-public-pipeline.R
@@ -0,0 +1,38 @@
+# Run the full data processing pipeline on a local server hosting raw data, ready to be pushed to public Github
+
+# 0. Set up -------
+pacman::p_load(here, purrr, dplyr)
+
+# Load the pipeline functions
+url_base <- "https://raw.githubusercontent.com/cmmid/gaza-response/main/R/data-pipeline/"
+pipeline_functions <- paste0(url_base,
+                             c("1-data_cleaning.R",
+                               "2-data_aggregation.R",
+                               "3-data-quality.R"))
+walk(pipeline_functions, ~ source(.))
+
+# Load data stored locally
+base_data <- readRDS(here("data", "processed", "df_base.RDS"))
+fup_data <- readRDS(here("data", "processed", "df_fup.RDS"))
+
+# 1. Clean data -----
+data_id <- clean_data(base_data, fup_data)
+
+# 2. Aggregate and calculate summaries by stratification -----
+group_cols <- c("overall", # specify stratifications
+                "sex", "agegroup", "governorate", "role", "children_feeding")
+
+data_id <- data_id |>
+  mutate(overall = "overall")
+
+# summary statistics over time windows
+# all time
+summary_all <- summarise_ids(data = data_id,
+                             group_cols = group_cols)
+
+# participants reporting in latest 3 day window
+
+# 3. Save locally ----
+saveRDS(current_summary_stats_table, here("results",
+                                            "current_summary_stats.csv"))
+# RDS data pushed to Github public repo

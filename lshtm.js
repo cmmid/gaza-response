0ac451cb838daa2b3a182003b7a0ab96f697e8d6
Ciara Judge <judge.ciara@gmail.com>
2025-08-06 16:50:10 +0100

    Adjust sidebar
    Sidebar now a checkbox that can select multiple options to stratify data a lot
    
    Still to do - adjust plotting code to take the stratifications and the new data and make all the plots

--- lshtm.js
+++ lshtm.js
@@ -1,19 +1,29 @@
 //filter in sidabar
 window.onload = function() {
   let widgets = document.querySelectorAll(".lshtm_widget");
-    
+
   document.querySelectorAll(".lshtm_widget_filter input")
   .forEach(input => {
     input.addEventListener("change", (e) => {
-      let stratum = input.value;
-      console.log("click "+stratum);
-      
+      // Get all checked checkbox values
+      let checkedStrata = Array.from(
+        document.querySelectorAll(".lshtm_widget_filter input:checked")
+      ).map(cb => cb.value);
+
+      // Sort them alphabetically (change this to any other sorting logic if needed)
+      checkedStrata.sort();
+
+      // Join with underscore
+      let stratum = checkedStrata.length > 0 ? checkedStrata.join("_") : "Overall";
+      console.log("Combined stratum string:", stratum);
+
       widgets.forEach(widget => {
         let widget_id = widget.querySelector(".lshtm_widget_identifier").id;
-        
+
         widget.querySelector(".visible").classList.remove("visible");
         widget.querySelector("#" + widget_id + "_" + stratum).classList.add("visible");
       });
     });
   });
+    document.querySelectorAll(".lshtm_widget_filter input")[0]?.dispatchEvent(new Event('change'));
 };

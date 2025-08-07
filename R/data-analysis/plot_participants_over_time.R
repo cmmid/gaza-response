33229218d3bfba61cd3dfcb3928a68b2a52a973b
Ciara Judge <judge.ciara@gmail.com>
2025-08-07 14:57:37 +0100

    Add plot for participants over time
    Plot showing cumulative participants over time (stratified if necessary) added. I'd like to put this on top beside key insights but a bit tricky with spacing, so currently it's just on the bottom of the dashboard

---
+++ R/data-analysis/plot_participants_over_time.R
@@ -0,0 +1,69 @@
+#...............................................................................
+### ++ MONITORING THE NUTRITIONAL STATUS OF HUMANITARIAN WORKERS IN GAZA +++ ###
+#...............................................................................
+
+#...............................................................................
+## ------ R SCRIPT TO GENERATE PLOTS OF PARTICIPANTS OVER TIME ----- ##
+#...............................................................................
+
+#...............................................................................
+### Preparatory steps
+#...............................................................................
+
+#...................................
+## Install or load required R packages
+pacman::p_load(
+  ggplot2,       # Visualise data
+  tidyverse)     # Tidyverse suite of packages
+
+#...............................................................................
+### Read in current summary statistics data
+#...............................................................................
+
+
+#...............................................................................
+### Plot
+#...............................................................................
+
+plot_participants_over_time <- function(data, strata = "Overall"){
+
+  # Filter data for the selected option
+  data_filter <- data[[tolower(strata)]] |>
+    pivot_wider(names_from = stat, values_from = value) %>%
+    dplyr::filter(!is.na(mean)) |>
+    filter(variable == "weight") |>
+    full_join(expand.grid(list(date = as.Date(min(data[[tolower(strata)]]$date) - 1),
+                               cohort_n = 0,
+                               label = unique(data[[tolower(strata)]]$label))))
+
+  if (strata == "Overall") {
+    # Generate plot
+    fig <- data_filter %>%
+      ggplot() +
+      geom_area(aes(x = date, y = cohort_n, fill = label, group = label),
+                position = "stack", alpha = 0.8, show.legend = F) +
+      labs(x = "Date",
+           y = "Participants",
+           fill = "Demographics") +
+      #theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+            strip.text.y = element_blank())
+  }
+
+  else {
+    fig <- data_filter %>%
+      ggplot() +
+      geom_area(aes(x = date, y = cohort_n, fill = label, group = label),
+                position = "stack", alpha = 0.8) +
+      labs(x = "Date",
+           y = "Participants",
+           fill = "Demographics") +
+      #theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+            strip.text.y = element_blank())
+  }
+
+
+  return(fig)
+
+}

1144945c03318f732a1da7b9b7234fcfa0bbec1e
kathsherratt <kathsherratt@gmail.com>
2025-08-08 12:52:26 +0100

    Update palette.R

--- R/data-analysis/palette.R
+++ R/data-analysis/palette.R
@@ -1,3 +1,6 @@
 # Colour palettes
-palette <- c(central = "#331941",
-             range ="#5e2b70")
+# https://venngage.com/tools/accessible-color-palette-generator
+palette <- c("lshtm" = "#0D5257",
+             "stat_central" = "#331941",
+             "stat_range" ="#5e2b70"
+             )

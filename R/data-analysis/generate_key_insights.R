4bc1e6cf7960d93bb8d3e99fd25b95014a405d2d
kathsherratt <kathsherratt@gmail.com>
2025-08-11 00:41:24 +0100

    update downstream to match changes to current summary
    Updates code for key insights and current summary plot to match changes in how the current summary is identified in the data. Splits data into whether it is the current summary for each variable (ie last 72h), or the summary for each date across the whole timeseries.

--- R/data-analysis/generate_key_insights.R
+++ R/data-analysis/generate_key_insights.R
@@ -15,18 +15,24 @@
 #...............................................................................
 
 
-generate_key_insights <- function(data) {
+generate_key_insights <- function(data, strata) {
+  data <- data[[tolower(strata)]]
+  data <- filter(data, variable == "weight_percent_change_prewar")
   params <- list(
     # Most recent date
-    latest_date = max(data$overall$date),
+    latest_date = data$current_summary_date[1],
     # N
-    cohort_size = max(data$overall$cohort_n),
+    cohort_size_current = max(data$cohort_id_enrolled, na.rm=TRUE),
+    cohort_size_alltime = max(data$cohort_id_enrolled_alltime, na.rm=TRUE),
     # Observations
-    observations = sum(distinct(filter(data$overall, variable == "weight" & stat == "mean"))$cohort_recorded),
+    observations = sum(distinct(filter(data, stat == "mean"))$cohort_obs_recorded),
 
-    median_change = round(filter(data$overall, date == max(date) & variable == "weight_percent_change_prewar" & stat == "median")$value, 2)
-
+    median_change = round(filter(data, stat == "median")$value, 1),
+    upper_change = round(filter(data, stat == "q3")$value, 1),
+    lower_change = round(filter(data, stat == "q1")$value, 1)
   )
+  params$cohort_percent_currently_participating <- params$cohort_size_current /
+    params$cohort_size_alltime * 100
+
   return(params)
 }
-

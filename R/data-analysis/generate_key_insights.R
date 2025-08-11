57ed9872aa8d9f6484212fe64f10ceb6df07eb0b
kathsherratt <kathsherratt@gmail.com>
2025-08-11 22:54:56 +0100

    fix key insights

--- R/data-analysis/generate_key_insights.R
+++ R/data-analysis/generate_key_insights.R
@@ -22,17 +22,15 @@ generate_key_insights <- function(data, strata) {
     # Most recent date
     latest_date = data$current_summary_date[1],
     # N
-    cohort_size_current = max(data$cohort_id_enrolled, na.rm=TRUE),
-    cohort_size_alltime = max(data$cohort_id_enrolled_alltime, na.rm=TRUE),
+    cohort_size = max(data$cohort_id_enrolled, na.rm=TRUE),
+    cohort_percent_participating = data$cohort_obs_recorded /
+      data$cohort_id_enrolled * 100,
     # Observations
     observations = sum(distinct(filter(data, stat == "mean"))$cohort_obs_recorded),
-
     median_change = round(filter(data, stat == "median")$value, 1),
     upper_change = round(filter(data, stat == "q3")$value, 1),
     lower_change = round(filter(data, stat == "q1")$value, 1)
   )
-  params$cohort_percent_currently_participating <- params$cohort_size_current /
-    params$cohort_size_alltime * 100
 
   return(params)
 }

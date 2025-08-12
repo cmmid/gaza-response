589827b6d403aba674c7c0c08213b0f4a2b9bb0d
kathsherratt <kathsherratt@gmail.com>
2025-08-12 07:30:07 +0100

    add error logging to pipeline

--- R/data-pipeline/run-public-pipeline.R
+++ R/data-pipeline/run-public-pipeline.R
@@ -3,6 +3,10 @@
 # Set up -------
 pacman::p_load(here, purrr, dplyr)
 
+# error logging
+log <- list()
+
+
 # Get wd from passed argument if ran on server
 .args = if(interactive()) here() else commandArgs(trailingOnly = TRUE)
 .args = setNames(.args, c("wd"))
@@ -25,6 +29,18 @@ walk(pipeline_functions, source)
 base_data <- readRDS(here("data", "processed", "df_base.RDS"))
 fup_data <- readRDS(here("data", "processed", "df_fup.RDS"))
 
+# check columns are correct
+expected_base <- c("id", "date", "organisation", "age", "sex", "governorate",
+              "role", "height", "weight_prewar", "weight", "children_feeding")
+log$base_cols_missing <- setdiff(expected_base, colnames(base_data))
+expected_fup <- c("id", "date", "weight")
+log$fup_cols_missing <- setdiff(expected_fup, colnames(fup_data))
+
+# add basic stats to log for checking
+log$n_participants_baseline <- length(unique(base_data$id))
+log$max_date <- max(fup_data$date)
+log$orgs <- unique(base_data$organisation)
+
 # Clean data -----
 data_id_daily <- clean_data(base_data, fup_data)
 
@@ -41,9 +57,12 @@ latest_date <- as.Date(max(data_id_daily$date, na.rm = TRUE))
 recent_days <- seq.Date(from = latest_date - 3,
                         length.out = 4, by = "day")
 data_id_current <- data_id_latest |>
-  filter(date %in% recent_days) |>
-  # set date to the future to use as a flag that this is the most recent record
-  #   (noting all group calculations include date so will not be double-counted)
+  filter(date %in% recent_days)
+log$recent_days <- unique(data_id_current$date)
+
+# set date to the future to use as a flag that this is the most recent record
+#   (noting all group calculations include date so will not be double-counted)
+data_id_current <- data_id_current |>
   mutate(date = Sys.Date() + 3650)
 
 # bind latest data with full time series
@@ -78,4 +97,8 @@ if(interactive()){
 output_file = ifelse(interactive(), here("data", "public", "summary-stats.RDS"), sprintf("%s/data/public/summary-stats.RDS", .args["wd"]))
 saveRDS(summary, output_file)
 
+output_log = ifelse(interactive(),
+                    here("data", "public", "log.RDS"),
+                    sprintf("%s/data/public/log.RDS", .args["wd"]))
+saveRDS(log, output_log)
 # RDS data pushed to Github public repo

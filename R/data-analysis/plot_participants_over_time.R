0f0cbd699c44f7672b6e59781b9f8ed314a02733
kathsherratt <kathsherratt@gmail.com>
2025-08-18 05:56:54 +0100

    Update plot_participants_over_time.R

--- R/data-analysis/plot_participants_over_time.R
+++ R/data-analysis/plot_participants_over_time.R
@@ -27,43 +27,24 @@ pacman::p_load(
 
 plot_participants_over_time <- function(data, strata = "Overall"){
 
-  # Filter data for the selected option
-  data_filter <- data[[tolower(strata)]] |>
-    pivot_wider(names_from = stat, values_from = value) %>%
-    dplyr::filter(!is.na(mean)) |>
-    filter(variable == "weight") |>
-    full_join(expand.grid(list(date = as.Date(min(data[[tolower(strata)]]$date) - 1),
-                               cohort_n = 0,
-                               label = unique(data[[tolower(strata)]]$label))))
+  plot_participants <- data[[tolower(strata)]] |>
+    filter(!is.na(date)) |>
+    filter(variable == "weight" & group == "overall" & stat == "median") |>
+    dplyr::select(-value) |>
+    mutate(cohort_id_followup = cohort_obs_recorded - cohort_id_new) |>
+    group_by(date, organisation, group, label) |>
+    pivot_longer(cols = c("cohort_obs_missing",
+                          "cohort_id_new",
+                          "cohort_id_followup")) |>
+    mutate(Participant = fct_rev(fct_recode(name,
+                                            "Lost to follow up"="cohort_obs_missing",
+                                            "New"="cohort_id_new",
+                                            "Repeated"="cohort_id_followup"))) |>
+    ggplot(aes(x = date)) +
+    geom_col(aes(y = value,
+                 fill = Participant, alpha = Participant)) +
+    labs(x = NULL, y = "Participant data")
+  ggplotly(plot_participants)
 
-  if (strata == "Overall") {
-    # Generate plot
-    fig <- data_filter %>%
-      ggplot() +
-      geom_area(aes(x = date, y = cohort_id_enrolled, fill = label, group = label),
-                position = "stack", alpha = 0.8, show.legend = F) +
-      labs(x = "Date",
-           y = "Participants",
-           fill = "Demographics") +
-      #theme_bw() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
-            strip.text.y = element_blank())
-  }
-
-  else {
-    fig <- data_filter %>%
-      ggplot() +
-      geom_area(aes(x = date, y = cohort_id_enrolled, fill = label, group = label),
-                position = "stack", alpha = 0.8) +
-      labs(x = "Date",
-           y = "Participants",
-           fill = "Demographics") +
-      #theme_bw() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
-            strip.text.y = element_blank())
-  }
-
-
-  return(fig)
-
+  return(plot_participants)
 }

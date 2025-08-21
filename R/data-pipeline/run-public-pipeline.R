74f0db811dd4c65cdface4fa0833d9654ac95715
kathsherratt <kathsherratt@gmail.com>
2025-08-21 12:51:56 +0100

    Update run-public-pipeline.R

--- R/data-pipeline/run-public-pipeline.R
+++ R/data-pipeline/run-public-pipeline.R
@@ -44,33 +44,15 @@ log$data_raw$orgs <- unique(base_data$organisation)
 
 # Clean data ------------------------------------------------------------
 # combine baseline and follow up data; calculate BMI and change
-data_id_daily <- clean_data(base_data, fup_data)
-# Recode factors
-data_id_daily <- data_id_daily |>
-  mutate(bmi_category_daily = bmi,
-         bmi_category_prewar = bmi_prewar)
 suppressWarnings(
-  {data_id_daily <- set_factors(df = data_id_daily,
-                                 factor_levels = c(data_dictionary$data_levels))
-  }
+  data_id_daily <- clean_data(base_data, fup_data, data_dictionary)
 )
-# Data quality ------------------------------------------------------------
-# Replace anomaly measurements as missing
-data_id_daily <- data_id_daily |>
-  mutate(across(contains(c("weight", "bmi")),
-                ~ if_else(weight_anomaly=="anomaly", NA, .x)))
-
 log$data_clean$nrow <- nrow(data_id_daily)
 log$data_clean$n_id <- length(unique(data_id_daily$id))
-
-# Replace all anomaly values as missing
-data_id_daily <- data_id_daily |>
-  mutate(across(where(is.factor),
-                ~ if_else(.x == "anomaly", NA, .x)))
-
-# Drop empty levels of organisation
-data_id_daily <- data_id_daily |>
-  mutate(organisation = fct_drop(organisation))
+log$data_clean$exclusion <- data_id_daily |>
+  group_by(organisation, anomaly) |>
+  summarise(n_records = n(),
+            n_participants = n_distinct(id))
 
 # Pre-processing for summaries ---------------------------------------------
 # add "Overall" organisation
@@ -82,70 +64,55 @@ data_id_daily <- data_id_daily |>
 data_id_daily <- data_id_daily |>
   mutate(overall = "overall")
 
-# Get only last available record -----------------------------------------
+# Most recent records -----------------------------------------
 # create a df with only last recorded observation by ID
 data_id_last <- data_id_daily |>
   filter(last_measurement)
-latest_date <- as.Date(max(data_id_daily$date, na.rm = TRUE))
-log$data_clean$latest_date <- latest_date
+log$data_latest$date_of_latest <- count(data_id_last, organisation, date)
 
-# set date to the future to use as a flag that this is the most recent record
+# # tabulate all participants by demography & data quality
+tables_latest <- tabulate_study(data_id_last, data_dictionary)
+log$data_latest$summary_tables <- tables_latest$Overall
+
+# strata summaries over time ------------------------------------------
+# set last date to the future to use as a flag that this is the most recent record
 #   (noting all group calculations include date so will not be double-counted)
 data_id_last <- data_id_last |>
   mutate(date = Sys.Date() + 3650)
 
-# table summary by most recent record --------------------------------------
-# tally characteristics across all participants
-# log$factor_count <- count_factors(data_id_daily)
-#
-# # tabulate all participants by organisation
-#TODO fix labels
-data_pretty <- data_id_last |>
-  dplyr::select(c("date", "id", "organisation",
-                  "age", "sex", "children_feeding",
-                  "role", "governorate",
-                  "participant_in_followup",
-                  "participant_cumulative_days_recorded",
-                  "participant_cumulative_days_enrolled")) |>
-  mutate(across(where(is.factor), fct_drop)) |>
-  rename(any_of(data_dictionary$variable_names))
-
-log$tab_baseline <- data_pretty |>
-  tbl_summary(by = "Organisation",
-              digits = all_continuous() ~ 1) |>
-  add_overall()
-
-# BMI categories
-log$tab_bmi_categories <- bmi_crosstab(data_id_last,
-                                       data_dictionary$bmi_category)
-
-# strata summaries ----------------------------------------------------
+# summarise by date, organisation, and group -----
 # bind last available record with full time series --
 data_id_aggregate <- bind_rows(data_id_daily, data_id_last)
-# TODO create the "group" and "label" column (at end of data_agg script) here. - tidyr::unite
 
-# summarise by date, organisation, and group -----
+group_cols <- c("overall", "age", "children_feeding",
+                "governorate", "role", "sex")
+group_cols <- map(group_cols,
+                  ~ c("date", "organisation", sort(.x)))
+
 # Create 2 levels of stratification
-group_cols <- c("overall", "age", "children_feeding", "governorate", "role", "sex")
 # group_cols <- combn(group_cols, 2, simplify = FALSE)
 # group_cols <- append(group_cols, as.list(c("overall", "age",
 #                                            "children_feeding", "governorate",
 #                                            "role", "sex")))
-group_cols <- append(group_cols, map(group_cols,
-                         ~ c("date", "organisation", sort(.x))))
+# group_cols <- append(group_cols, map(group_cols,
+#                          ~ c("date", "organisation", sort(.x))))
 
 #' Do not print all messages
 suppressMessages({
-    summary <- map(group_cols,
+  summary <- map(group_cols,
                     ~ data_id_aggregate |>
                       summarise_strata(group_cols = .x)) |>
       clean_aggregated_data()
   })
+
 log$data_summary$overall_latest_date <- max(summary$Overall$overall$date,
                                             na.rm=TRUE)
 log$data_summary$overall_sample <- dplyr::slice_sample(summary$Overall$overall, prop = 0.1)
 
 # save ----------------------
+output_tables = sprintf("%s/data/public/summary-tables.RDS",.args["wd"])
+saveRDS(tables_latest, output_tables)
+
 output_file = sprintf("%s/data/public/summary-stats.RDS", .args["wd"])
 saveRDS(summary, output_file)
 

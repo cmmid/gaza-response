882f42007657f4f15231592eb77930375d13d4f4
kathsherratt <kathsherratt@gmail.com>
2025-08-22 10:11:49 +0100

    generate key insight text

--- R/data-analysis/generate_key_insights.R
+++ R/data-analysis/generate_key_insights.R
@@ -13,47 +13,49 @@
 #...............................................................................
 ### Generate list of key insights
 #...............................................................................
+# eg
+# '
+# A typical change in weight of `r summary_text[summary_text$variable =="weight_change_unit_prewar","median_iqr"]`kg (median, 25-75% range)
+# '
 
-
-generate_key_insights <- function(data, strata) {
-  data <- data[[tolower(strata)]]
-
-  summary_table <- data |>
-    filter(variable %in% c("bmi_unit_change_prewar",
-                           "weight_unit_change_prewar",
-                           "bmi_percent_change_prewar",
-                           "weight_percent_change_prewar")) |>
+generate_key_insights <- function(data) {
+  summary_text_tab <- data |>
+    group_by(label) |>
     pivot_wider(names_from = stat, values_from = value) |>
     dplyr::select(date, organisation, label,
-                  variable, median, q1, q3)
+                  cohort_obs_recorded, cohort_id_followup_record,
+                  variable, median, q1, q3, count) |>
+    mutate(across(where(is.numeric), round, 1),
+           median_iqr = paste0(median, " (", q1, "-", q3, ")"),
+           bmi_pc = paste0(count, " (",
+                                round((count / cohort_obs_recorded*100),0), "%)"))
 
-  summary_cohort <- data |>
-    distinct(organisation,
-             cohort_id_enrolled, participant_in_followup,
-             group, label) |>
-    pivot_longer(contains("cohort"), names_to = "variable")
-
-  summary_insights <- left_join(summary_table, summary_cohort, by = c("label")) |>
-    drop_na() |>
-    mutate(across(where(is.numeric), ~round(., digits=1))) |>
-    mutate("Median (25-75% range)" = paste0(median, " (", q1, "-", q3, ")")) |>
-    dplyr::select("Organisation" = organisation,
-                  "Strata" = label, "Indicator" = variable, "Median (25-75% range)")
-
-  params <- list(
-    # Most recent date
-    latest_date = max(data$current_summary_date, na.rm=TRUE),
-    # N
-    cohort_size = max(data$cohort_id_enrolled, na.rm=TRUE),
-    # Observations
-    #
-    median_change = round(filter(data, stat == "median")$value, 0),
-    upper_change = round(filter(data, stat == "q3")$value, 0),
-    lower_change = round(filter(data, stat == "q1")$value, 0)
-    #
-
+  key_text <- list(
+    # Number of ppts (date range)
+    cohort_n = max(data$cohort_id_recorded),
+    # Number in follow up
+    cohort_fup = max(data$cohort_id_followup_ever),
+    # date range of current summary
+    date_range = paste0(min(log$data_latest$date_of_latest$date),
+                        " - ", max(log$data_latest$date_of_latest$date)),
+    # Median weight change in kg (IQR) since pre-war
+    wt_chg_iqr_kg = summary_text_tab[summary_text_tab$variable == "weight_change_unit_prewar","median_iqr"],
+    # Median weight change in %
+    wt_chg_iqr_pct = summary_text_tab[summary_text_tab$variable == "weight_change_percent_prewar","median_iqr"],
+    # PPts in follow up, % weight change in kg (IQR) since entry
+    wt_chg_iqr_kg = summary_text_tab[summary_text_tab$variable == "weight_change_unit_entry","median_iqr"],
+    # Median weight change in %
+    wt_chg_iqr_pct = summary_text_tab[summary_text_tab$variable == "weight_change_percent_prewar","median_iqr"],
+    # % in "underweight" category
+    cohort_underwt_pc = summary_text_tab[summary_text_tab$variable == "bmi_category_daily_Underweight","bmi_pc"]
+    # % in "underweight" category pre-war
+    cohort_underwt_pc_prewar = summary_text_tab[summary_text_tab$variable == "bmi_category_prewar_Underweight","bmi_pc"]
   )
 
-  return(params)
-}
+  # average length of time in study
+  # = cohort_persondays / cohort_obs_recorded
 
+  # Daily rate of weight loss since study entry
+
+  return(key_text)
+}

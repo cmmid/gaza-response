b792197012f71fa7680fed8df9ecfe0df36d2627
kathsherratt <kathsherratt@gmail.com>
2025-09-02 14:30:08 +0100

    only pipeline files

--- R/data-pipeline/0-data-dictionary.R
+++ R/data-pipeline/0-data-dictionary.R
@@ -3,14 +3,111 @@
 ### Structured as a named list which gets stored as an R object on the public repo
 ### and can be used to properly factor the data when rendering the dashboard
 #...............................................................................
+# examples:
+# df$variable <- forcats::fct_recode(df$variable,
+#                                   !!!data_dictionary$variable_names))
 
-data_dict <- list()
-data_dict[["sex"]] <- c("male", "female", "other/prefer not to answer")
-data_dict[["governorate"]] <- c("North Gaza", "Gaza City", "Deir Al Balah", "Khan Yunis", "Rafah")
-data_dict[["organisation"]] <- c("Save the Children International", "MSF-OCBA", "UNRWA")
-data_dict[["agegroup"]] <- c("Under 30 years", "30-44 years", "Over 45 years")
-data_dict[["category"]] <- c("Underweight", "Normal", "Overweight", "Obese")
-data_dict[["role"]] <- c("expatriate", "national staff member", "consultant or contractor", "casual staff/daily worker", "other", "prefer not to answer")
+library(forcats)
+library(dplyr)
 
-saveRDS(data_dict, "./data/data_dictionary.RDS")
+set_data_dictionary <- function() {
 
+  dictionary <- list(
+    factor_cols = c("overall", "organisation",
+                    "sex", "age", "children_feeding", "role", "governorate",
+                    "bmi_category_daily", "bmi_category_prewar",
+                    "strata", "stratum", "variable_name", "anomaly"),
+    data_levels = c(
+  # Categories within data -------------------------
+    # sex
+    "Male" = "male",
+    "Female" = "female",
+    "Other" = "other/prefer not to answer",
+    # age
+    "Age under 30" = "Age under 30",
+    "Age 30-45" = "Age 30-45",
+    "Age over 45" = "Age over 45",
+    "Missing" = "anomaly",
+    # dependants
+    "0 children" = "0",
+    "1 child" = "1",
+    "2 children" = "2",
+    "3+ children" = "3+",
+    "Missing" = "NA",
+    # role
+    "Staff expatriate" = "expatriate",
+    "Staff national" = "national staff member",
+    "Staff contractor" = "consultant or contractor",
+    "Casual worker" = "casual staff/daily worker",
+    "Other" = "other",
+    "Other" = "prefer not to answer",
+    # organisation
+    "UNRWA" = "UNRWA",
+    "Save the Children International" = "Save the Children International",
+    # governorate
+    "North Gaza" = "North Gaza",
+    "Gaza City" = "Gaza City",
+    "Deir Al Balah" = "Deir Al Balah",
+    "Khan Yunis" = "Khan Yunis",
+    "Rafah" = "Rafah",
+    # ----- created data ------------------
+    # BMI categories
+    "Underweight" = "Underweight",
+    "Normal" = "Normal",
+    "Overweight" = "Overweight",
+    "Obese" = "Obese",
+    "Missing" = "NA",
+    # anomaly classification
+    "Excluded (>10% daily rate of weight change since entry)" = "excluded_rate",
+    "Excluded (BMI <10 or >60)" = "excluded_bmi",
+    "Excluded (weight <30kg or >180kg)" = "excluded_weight",
+    "Missing" = "missing",
+    "Excluded (conflicting double record at study entry)" = "excluded_conflict",
+    "Included" = "included",
+    # ---------- Variable name labels -----------------------
+    # strata names
+    "Overall" = "overall",
+    "Organisation" = "organisation",
+    "Sex" = "sex",
+    "Age" = "age",
+    "Dependent children" = "children_feeding",
+    "Staff role" = "role",
+    "Governorate" = "governorate",
+    # -----
+    # cohort participation
+    "Date" = "date",
+    "Date of study entry" = "date_entry",
+    "Participant timepoint" = "participant_timepoint",
+    "Demographic strata" = "strata",
+    "Stratum" = "stratum",
+    "Number of participants" = "cohort_id_recorded",
+    "Number of measurements" = "cohort_obs_recorded",
+    "Number of newly enrolled participants" = "cohort_id_new",
+    "Number of follow up measurements" = "cohort_id_followup_record",
+    "Number of participants ever in follow up" = "cohort_id_followup_ever",
+    "Number of participants missing measurements" = "cohort_obs_missing",
+    "Cumulative participant days since entry" = "cohort_persondays",
+    "Cumulative participant days since entry among follow-up" = "cohort_persondays_followup",
+    "Participant days since study entry" = "participant_cumulative_days_enrolled",
+    "Participant days with recorded weight" = "participant_cumulative_days_recorded",
+    "Exclusion criteria" = "anomaly",
+  # measurements
+    # weight
+      "Weight, kg" = "weight_daily",
+      "Weight change since start of war (kg)" = "weight_change_unit_prewar",
+      "Weight change since start of war (%)" = "weight_change_percent_prewar",
+      "Weight change since study entry (%)" = "weight_change_percent_entry",
+      "Weight daily rate of change since study entry (%/day)" = "weight_change_percent_daily_rate_entry",
+    # BMI
+      "BMI, kg/m²" = "bmi_daily",
+      "BMI change since start of war" = "bmi_change_unit_prewar",
+      "BMI change since start of war (%)" = "bmi_change_percent_prewar",
+      "BMI change since enrollment (%)" = "bmi_change_percent_entry",
+      "BMI daily rate of change since study entry (%/day)" = "bmi_change_percent_daily_rate_entry",
+    "Pre-war BMI" = "bmi_category_prewar",
+  "Current BMI" = "bmi_category_daily"
+    )
+    )
+
+    return(dictionary)
+  }
--- R/data-pipeline/2-data_aggregation.R
+++ R/data-pipeline/2-data_aggregation.R
@@ -1,49 +1,142 @@
-# Functions to aggregate data at each intersection of stratifying variables
+# Functions to aggregate data : overall characteristics, and at each intersection of stratifying variables
 #
 # Example
 # base_data <- readRDS(here("data", "processed", "df_base.RDS"))
 # fup_data <- readRDS(here("data", "processed", "df_fup.RDS"))
 # data_id <- clean_data(base_data, fup_data)
 
-pacman::p_load(dplyr, tidyr, purrr)
+#TODO tidy data: produce dataframe with
+# date - group - N - timepoint - indicator (cohort, bmi, weight) - stat - value
+
+pacman::p_load(dplyr, tidyr, purrr, gtsummary, janitor, stringr)
+
+# tabulate summary statistics across cohort ----------------------------
+# Table 1 summary
+tabulate_study <- function(data, strata, data_dictionary) {
+  organisations <- unique(data$organisation)
+
+  # Data quality
+  quality_df <- data |>
+    mutate(participant_cumulative_days_recorded = as.numeric(participant_cumulative_days_recorded)) |>
+    dplyr::select("Organisation" = "organisation",
+                  "Number of observations by timepoint" = participant_timepoint,
+                  "Number of observations per participant" = participant_cumulative_days_recorded,
+                  "Excluded observations" = anomaly
+                  ) |>
+    rename(any_of(c(data_dictionary$data_levels)))
+
+  tab_quality <- map(organisations,
+                   ~ quality_df |>
+                     filter(Organisation == .x) |>
+                     tbl_summary(
+                       include = -c("Organisation"),
+                       missing = "ifany", missing_text = "Missing") |>
+                     modify_header(label ~ "**Observations**") |>
+                     modify_source_note(source_note =
+                     "Exclusion criteria indicate observation recorded but outside of valid range, and treated as missing.
+                     Valid ranges: weight, 30-180kg; BMI: 10-60; daily rate of change in weight since study entry over +/-10%; age, 16-99 years; dependent children, 0-20") |>
+                     as_gt()
+                   )
+  names(tab_quality) <- organisations
+
+  # Demographic strata
+  strata_df <- data |>
+    dplyr::select("organisation",
+                  "Participant timepoint" = "participant_timepoint",
+                  any_of(c(!!!strata))) |>
+    rename(any_of(c(data_dictionary$data_levels)))
+  tab_strata <- map(organisations,
+                 ~ strata_df |>
+                   filter(Organisation == .x) |>
+                   tbl_summary(by = "Participant timepoint",
+                               percent = "column",
+                               include = -c("Organisation", "overall"),
+                               missing = "ifany",
+                               missing_text = "Missing") |>
+                   add_overall() |>
+                   as_gt()
+                   )
+  names(tab_strata) <- organisations
+
+    # BMI category crosstab
+    tab_bmi <- map(organisations,
+          ~ data |>
+            filter(organisation == .x) |>
+            tbl_cross(row = "bmi_category_prewar",
+                      col = "bmi_category_daily",
+                      percent = "row",
+                      missing = "no",
+                      digits = 0,
+                      label = list("bmi_category_daily" = "Current BMI",
+                                   "bmi_category_prewar" = "Pre-war BMI")) |>
+            as_gt()
+    )
+    names(tab_bmi) <- unique(data$organisation)
+
+    study_tables <- list("data_quality" = tab_quality,
+                         "demographic_strata" = tab_strata,
+                         "bmi_crosstab" = tab_bmi)
+    org_tables <- list_transpose(study_tables)
+
+    return(org_tables)
+}
 
 # summarise by any given strata ------------------------------------
-summarise_ids <- function(data, group_cols) {
+summarise_strata <- function(data, strata) {
   #if(interactive()) print(group_cols)
+
+  # create single grouping id
+  data <- data |>
+    mutate(strata = {{ strata }},
+           stratum = paste(!!!syms(strata)))
 
   # summarise participants per group -----
   df_participants <- data |>
-    group_by(across(all_of(group_cols))) |>
+    group_by(date, organisation, strata, stratum) |>
     summarise(
-      # participants enrolled ---
-      cohort_id_enrolled = length(unique(id)),
-      # cohort new joiners ---
-      cohort_id_new = sum(date == date_first_measurement),
-      # daily observations ---
-      # number of recorded weights, denominator: cohort_n
-      cohort_obs_recorded = sum(!is.na(weight)),
-      # missing weight among all enrolled, denominator: cohort_n
-      cohort_obs_missing = sum(is.na(weight)),
-      # anomalous weight among recorded weights, denominator: cohort_obs_recorded
-      cohort_obs_anomalous = sum(weight_anomaly),
+      # participants ---
+      # -- all observed
+      cohort_id_recorded = sum(participant_recorded),
+      # -- cohort new joiners
+      cohort_id_new = sum(participant_timepoint == "Study entry"),
+      # -- cohort in follow up
+      cohort_id_followup_record = sum(participant_timepoint == "Follow up"),
+      # -- cohort in follow up
+      cohort_id_followup_ever = sum(participant_in_followup),
+      # -- time in study across all participants
+      cohort_persondays = sum(participant_cumulative_days_enrolled),
+      # -- time in study among those in follow up
+      cohort_persondays_followup = sum(cohort_persondays - cohort_id_new +
+                                         cohort_id_followup_ever),
+      # observations
+      # number of recorded weights
+      cohort_obs_recorded = sum(!is.na(weight_daily)),
+      # missing/anomalous weight among recorded
+      cohort_obs_missing = sum(is.na(weight_daily)),
+      #
       .groups = "drop"
       )
 
   # summarise observed metrics -----
-  ## drop anomalous and missing observations
-  # data <- data |>
-  #   filter(!is.na(weight) & !weight_anomaly)
-
   # averages
   df_centraltendency <- data |>
-    group_by(across(all_of(group_cols))) |>
+    group_by(date, organisation, strata, stratum) |>
+    arrange(date, .by_group = TRUE) |>
     summarise(
-      across(c("weight",
-               "weight_percent_change_firstmeasurement",
-               "weight_percent_change_prewar",
+      across(any_of(c("weight_daily",
+               "weight_change_unit_prewar",
+               "weight_change_percent_entry",
+               "weight_change_percent_prewar",
+               "weight_change_percent_daily_rate_entry",
+               #
                "bmi",
-               "bmi_percent_change_firstmeasurement",
-               "bmi_percent_change_prewar"),
+               "bmi_change_unit_prewar",
+               "bmi_change_percent_entry",
+               "bmi_change_percent_prewar",
+               "bmi_change_percent_daily_rate_entry",
+               #
+               "participant_cumulative_days_enrolled"
+               )),
              .fns = list(
                mean = ~ mean(., na.rm = TRUE),
                median = ~ median(., na.rm = TRUE),
@@ -52,65 +145,33 @@ summarise_ids <- function(data, group_cols) {
              .names = "{.col}.{.fn}"),
       .groups = "drop"
     ) |>
-    pivot_longer(cols = -group_cols) %>%
+    pivot_longer(cols = -c(date, organisation, strata, stratum)) %>%
     separate(name, into = c("variable", "stat"), sep = "\\.")
 
   # BMI by category
   df_bmi_count <- data |>
-    filter(!is.na(bmi)) |>
-    group_by(across(all_of(c(group_cols)))) |>
+    filter(!is.na(bmi_daily)) |>
+    group_by(date, organisation, strata, stratum) |>
     pivot_longer(cols = contains("bmi_category"),
-                 names_to = "bmi_period", values_to = "bmi_category") |>
-    group_by(across(all_of(c(group_cols, "bmi_period", "bmi_category")))) |>
+                 names_to = "bmi_period",
+                 values_to = "bmi_category") |>
+    group_by(date, organisation, strata, stratum,
+             bmi_period, bmi_category) |>
     count(name = "value") |>
     mutate(stat = "count",
            variable = paste0(bmi_period, "_", bmi_category))
-  # |>
-  #   # TODO this might need updating to use the data dictionary, as nesting() only completes based on what's in the data
-  #   complete(nesting(!!!syms(group_cols)),
-  #            stat, variable,
-  #            fill = list(value = 0))
 
   # combine summaries -----
   df_summary <- bind_rows(df_centraltendency,
                           df_bmi_count) |>
     left_join(df_participants,
-              by = group_cols) |>
+              by = c("date", "organisation",
+                     "strata", "stratum")) |>
     ungroup() |>
-    # create single grouping id
-    mutate(group = paste(group_cols, collapse = "-"),
-            label = pmap_chr(across(all_of(setdiff(group_cols, c("date", "organisation")))), ~ paste(..., sep = ", ")))
+    mutate(date_summarised = Sys.Date())
+
+  df_summary <- df_summary |>
+    filter(stratum != "NA")
 
   return(df_summary)
-}
-
-clean_aggregated_data <- function(summary_list) {
-
-  summary_df <- list_rbind(summary_list) |>
-    ungroup()
-
-  # label current summary stats - setting "date" to NA (as this is a summary of multiple dates),
-  #   and marking these records with "current_summary_date" = latest date in the data
-  latest_date <- max(summary_df |> filter(date <= Sys.Date()) |> pull(date), na.rm=TRUE)
-  summary_df <- summary_df |>
-    mutate(current_summary_date = as.Date(if_else(date > Sys.Date(),
-                                         latest_date, NA)),
-           date = replace(date, date > Sys.Date(), NA))
-
-
-  # drop "other" sex category
-  summary_df <- summary_df |>
-    filter(!grepl("other/prefer not to share", sex))
-
-  # split into a list indexed by organisation
-  org_split <- summary_df |>
-    mutate(organisation = if_else(is.na(organisation), "all", organisation)) |>
-    mutate(group = str_replace_all(group, "date-organisation-", "")) |>
-    mutate(group = str_replace_all(group, "date-", "")) |>
-    dplyr::select(-overall)
-  org_split <- split(org_split, org_split$organisation)
-  org_split <- map(org_split,
-                   ~ split(., .$group))
-
-  return(org_split)
 }
